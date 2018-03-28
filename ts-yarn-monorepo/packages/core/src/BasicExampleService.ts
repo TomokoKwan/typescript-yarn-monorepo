4fade5e7e3c3889aff02b67437647a06a4fc78e7
Ali Hosseini Khayat <ali-hk@users.noreply.github.com>
2018-03-28 11:44:39 -0700

    add BasicExampleService

---
+++ ts-yarn-monorepo/packages/core/src/BasicExampleService.ts
@@ -0,0 +1,12 @@
+import { IExampleService } from "./IExampleService";
+
+export class BasicExampleService implements IExampleService {
+    doExampleWork(input: string): Uint8Array {
+        let result: Uint8Array = new Uint8Array(input.length);
+        for (let i: number = 0; i < input.length; i++) {
+            result[i] = input.charCodeAt(i);
+        }
+
+        return result;
+    }
+}
