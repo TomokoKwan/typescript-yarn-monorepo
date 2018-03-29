03b70b5e72db78c278880c04d9f3b825caab7bee
Ali Hosseini Khayat <ali-hk@users.noreply.github.com>
2018-03-29 08:20:08 -0700

    add usage of BasicExampleService

--- ts-yarn-monorepo/packages/web/src/App.tsx
+++ ts-yarn-monorepo/packages/web/src/App.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
+import { IExampleService, BasicExampleService } from "@ts-example/core";
 import "./App.css";
 
 const logo = require("./logo.svg");
 
 class App extends React.Component {
   render() {
+    const exampleService: IExampleService = new BasicExampleService();
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +15,9 @@ class App extends React.Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>
+            {exampleService.doExampleWork("Hello World!")}
         </p>
       </div>
     );
