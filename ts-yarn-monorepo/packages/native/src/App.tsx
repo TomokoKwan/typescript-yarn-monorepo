0d1b36a6536da7bab243e2348b32dd3cd64a0d4c
Ali Hosseini Khayat <ali-hk@users.noreply.github.com>
2018-03-29 19:18:06 -0700

    fix all ' should be "

--- ts-yarn-monorepo/packages/native/src/App.tsx
+++ ts-yarn-monorepo/packages/native/src/App.tsx
@@ -1,21 +1,21 @@
-import * as React from 'react';
+import * as React from "react";
 import {
   Platform,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from "react-native";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const instructions: string = Platform.select({
+  ios: "Press Cmd+R to reload,\n" +
+    "Cmd+D or shake for dev menu",
+  android: "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu",
 });
 
 type Props = {};
 export default class App extends React.Component<Props> {
-  render() {
+  render(): JSX.Element {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -32,21 +32,21 @@ export default class App extends React.Component<Props> {
   }
 }
 
-const styles = StyleSheet.create({
+const styles: any = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10,
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
+    textAlign: "center",
+    color: "#333333",
     marginBottom: 5,
   },
 });
